d4a52d05faefc0490381052308c8c3df230e0a85
Arnaldo Oliveira <ago120682@gmail.com>
2022-10-22 15:22:58 -0300

    Revert "first commit"
    This reverts commit 626611bd053aa05a2081658514cf8302805f4e98.

--- server/src/TheMovie.Infrastructure.Data/Externsions/EntityTypeConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace TheMovie.Infrastructure.Data.Externsions
-{
-    public abstract class EntityTypeConfiguration<TEntity> where TEntity : class
-    {
-        public abstract void Map(EntityTypeBuilder<TEntity> builder);
-    }
-}
